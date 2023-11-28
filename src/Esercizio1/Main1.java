fe0304a8adb3aa4786af0cacad24144a0ff19ec9
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2023-11-28 15:36:20 +0100

    Secondo Commit

--- src/Esercizio1/Main1.java
+++ src/Esercizio1/Main1.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main1 {
+    //PUNTO 1
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -16,6 +17,24 @@ public class Main1 {
             return true; // se il numero di caratteri è pari
         } else {
             return false; // se il numero di caratteri è dispari
+        }
+    }
+    //PUNTO 2
+    public static void mainUno(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci un anno: ");
+        int inputAnno = scanner.nextInt();
+
+        boolean risultato = annoBisestile(inputAnno);
+        System.out.println(risultato);
+    }
+
+    public static boolean annoBisestile(int anno) {
+        if ((anno % 4 == 0 && anno % 100 != 0) || (anno % 400 == 0)) {
+            return true; //l'anno inserito è un anno bisestile
+        } else {
+            return false; //l'anno inserito non è un anno bisestile
         }
     }
 }
