1754b9d05a2d656e4b1ec98cabc2b848e6b0a999
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2023-11-28 15:14:17 +0100

    Primo Commit

--- src/Esercizio1/Main1.java
+++ src/Esercizio1/Main1.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
 
+public class Main1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci una stringa: ");
+        String inputString = scanner.nextLine();
+
+        boolean risultato = stringaPariDispari(inputString);
+        System.out.println(risultato);
+    }
+
+    public static boolean stringaPariDispari(String input) {
+        if (input.length() % 2 == 0) {
+            return true; // se il numero di caratteri è pari
+        } else {
+            return false; // se il numero di caratteri è dispari
+        }
+    }
+}
